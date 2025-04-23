26f879d1d1044d3205585c245ee7eb593244dcb2
Олег bc2024-5 <olegchaplia2006@gmail.com>
2025-04-23 19:44:58 +0300

    update data movies

--- src/data/movies.js
+++ src/data/movies.js
@@ -1,50 +1,50 @@
 export const movies = [
     {
         id: 1,
-        title: "Inception",
-        description: "A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.",
-        genre: "Sci-Fi",
-        showtime: "2025-12-15 19:30",
-        poster: "https://images.pexels.com/photos/7991579/pexels-photo-7991579.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        title: "Avengers: Endgame",
+        description: "After the devastating events of Infinity War, the Avengers assemble once more to undo Thanos' actions and restore balance.",
+        genre: "Action/Sci-Fi",
+        showtime: "2025-7-15 19:30",
+        poster: "https://disney.images.edge.bamgrid.com/ripcut-delivery/v1/variant/disney/7b350a2f-0b3e-4033-8125-34c4d67e3bbe?/scale?width=1200&aspectRatio=1.78&format=webp"
     },
     {
         id: 2,
-        title: "The Shawshank Redemption",
-        description: "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
-        genre: "Drama",
-        showtime: "2025-12-16 18:00",
-        poster: "https://images.pexels.com/photos/2873486/pexels-photo-2873486.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        title: "Spider-Man: No Way Home",
+        description: "Peter Parker asks Doctor Strange for help after his identity is revealed, leading to chaos in the multiverse.",
+        genre: "Action/Fantasy",
+        showtime: "2025-7-16 18:00",
+        poster: "https://play-lh.googleusercontent.com/Odv1QnJMznTD_SRuatFVgE6b2Lgt_BGb8TUBMjsNl-gJi3bnxLXgnusGXWHkFGihsJdTde9ucIhNwMPHbkAF=w240-h480-rw"
     },
     {
         id: 3,
-        title: "The Dark Knight",
-        description: "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
-        genre: "Action",
-        showtime: "2025-12-17 20:15",
-        poster: "https://images.pexels.com/photos/5662857/pexels-photo-5662857.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        title: "Iron Man",
+        description: "After being held captive, Tony Stark builds a high-tech suit of armor and becomes the hero known as Iron Man.",
+        genre: "Action/Sci-Fi",
+        showtime: "2025-7-17 20:15",
+        poster: "https://disney.images.edge.bamgrid.com/ripcut-delivery/v1/variant/disney/d055997a-b9ae-44bb-81ec-7e0ed58b8f6f?/scale?width=1200&aspectRatio=1.78&format=webp"
     },
     {
         id: 4,
-        title: "Pulp Fiction",
-        description: "The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption.",
-        genre: "Crime",
-        showtime: "2025-12-18 21:00",
-        poster: "https://images.pexels.com/photos/8108063/pexels-photo-8108063.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        title: "Black Panther",
+        description: "T'Challa returns to Wakanda to take his rightful place as king but faces a powerful enemy from his nation's past.",
+        genre: "Action/Adventure",
+        showtime: "2025-7-18 21:00",
+        poster: "https://cdn.kobo.com/book-images/7e060fa5-3a91-4ed1-94a3-57274b53e233/1200/1200/False/marvel-s-black-panther-4.jpg"
     },
     {
         id: 5,
-        title: "The Lord of the Rings",
-        description: "A meek Hobbit from the Shire and eight companions set out on a journey to destroy the powerful One Ring and save Middle-earth from the Dark Lord Sauron.",
-        genre: "Fantasy",
-        showtime: "2025-12-19 17:45",
-        poster: "https://images.pexels.com/photos/3224156/pexels-photo-3224156.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        title: "Doctor Strange in the Multiverse of Madness",
+        description: "Doctor Strange travels into the multiverse to confront mysterious enemies — and alternate versions of himself.",
+        genre: "Fantasy/Sci-Fi",
+        showtime: "2025-7-19 17:45",
+        poster: "https://d23.com/app/uploads/2022/05/00_STRANGE-780x440.jpg"
     },
     {
         id: 6,
-        title: "Forrest Gump",
-        description: "The presidencies of Kennedy and Johnson, the Vietnam War, the Watergate scandal and other historical events unfold from the perspective of an Alabama man with an IQ of 75, whose only desire is to be reunited with his childhood sweetheart.",
-        genre: "Drama",
-        showtime: "2025-12-20 19:00",
-        poster: "https://images.pexels.com/photos/7234256/pexels-photo-7234256.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        title: "Guardians of the Galaxy",
+        description: "A ragtag group of cosmic misfits unites to save the galaxy from a powerful villain seeking ultimate control.",
+        genre: "Sci-Fi/Action",
+        showtime: "2025-7-20 19:00",
+        poster: "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/C1C0181ED7342217394E5AC940003A8653E3311B6016194749B29A0FE3F238EB/scale?width=1200&aspectRatio=1.78&format=webp"
     }
 ];
